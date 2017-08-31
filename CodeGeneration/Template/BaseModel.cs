fdb9c8728eb2388a51bfeb2780da691ec175612c
孙诚 <sunchengsuncheng@gmail.com>
2017-08-31 10:02:44 +0800

    add: 代码模板文件

---
+++ CodeGeneration/Template/BaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Template
+{
+    public class BaseModel
+    {
+        protected static DateTime ToDateTime(string str)
+        {
+            DateTime data;
+            try
+            {
+                data = Convert.ToDateTime(str);
+            }
+            catch (Exception)
+            {
+                data = Convert.ToDateTime("1900-01-01");
+            }
+            return data;
+        }
+    }
+}
