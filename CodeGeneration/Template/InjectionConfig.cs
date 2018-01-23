d31f07dd9a1ec0341849af4a936eaca1cc767e55
suncheng <sunchengsuncheng@gmail.com>
2018-01-23 11:12:25 +0800

    使用Autofac

---
+++ CodeGeneration/Template/InjectionConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Reflection;
+using Autofac;
+using Autofac.Integration.Mvc;
+{0}
+
+namespace Web
+{
+    public class InjectionConfig
+    {
+        public static void Init()
+        {
+            var builder = new ContainerBuilder();
+            // 注册Controllers
+            builder.RegisterControllers(Assembly.GetExecutingAssembly())
+                   .PropertiesAutowired()
+                   .InstancePerLifetimeScope();
+
+            #region 注册类别
+            {1}
+            #endregion 注册类别END
+
+            //通过容器配置生成容器.   
+            var container = builder.Build();
+            //提供给MVC  
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
